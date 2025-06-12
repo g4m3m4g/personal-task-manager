030c5885326438c132b9fcce1e0e1dceec1bcadc
Francis <gallowsgame_official@hotmail.com>
2025-06-13 00:51:03 +0700

    add: Dockerfile, nginx.conf

--- server/index.js
+++ server/index.js
@@ -20,7 +20,10 @@ app.use("/api/auth", authRoutes);
 
 // DB & Server Init
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("MongoDB connected");
     app.listen(process.env.PORT, () => {
